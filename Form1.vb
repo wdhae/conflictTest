944bd2d3f3151b3161f39db10bf8df20f34bcc3c
wdhae <willem@tcmb.eu>
2012-01-04 15:38:12 +0100

    my event handlers

--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
 
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MessageBox.Show("Dit is de event handler van willem !")
+    End Sub
 End Class
